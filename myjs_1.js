eff3b2327785db5208a37b2d01c3470835816c1c
RefainZero <1399811201@qq.com>
2018-01-30 21:28:29 +0800

    javascript变量、输出语句，函数调用绑定事件练习

---
+++ myjs_1.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2018/1/30.
+ */
+document.write("welcome my world!!");
+//变量部分练习
+var mychar="i love you!";
+document.write("<br />"+mychar);
+/**
+ * 多行注释
+ */
+//单行注释练习
+document.write("我是单行注释。。。。。");
+//判断语句练习
+var a=90;
+if(a>=90){
+    document.write("考的不错！");
+}else{
+    document.write("重新来过！！");
+}
